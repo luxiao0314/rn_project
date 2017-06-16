6cbfb82bbf96d5b03852ca4013a0b2d1adb2e100
Luthor <luxiao418@pingan.com.cn>
2017-06-16 18:15:07 +0800

    commit

--- index.android.js
+++ index.android.js
@@ -5,5 +5,6 @@
 global.__IOS__ = false
 global.__ANDROID__ = true
 
-require('./react_native/burqa/common/GlobalContants')
-require('./react_native/burqa/App')
+require('./react_native/burqa/common/GlobalContants');
+require('./react_native/burqa/App');
+require('./react_native/burqa/Root');
--- index.ios.js
+++ index.ios.js
@@ -5,5 +5,6 @@
 global.__IOS__ = true
 global.__ANDROID__ = false
 
-require('./react_native/burqa/common/GlobalContants')
-require('./react_native/burqa/App')
+require('./react_native/burqa/common/GlobalContants');
+require('./react_native/burqa/App');
+require('./react_native/burqa/Root');
